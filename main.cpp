b7642703774b20fd2f6c5767d753ab7cd7924b48
nepaulius <pauliuscern.lt@gmail.com>
2019-02-07 21:11:07 +0200

    goal

--- main.cpp
+++ main.cpp
@@ -8,18 +8,23 @@ int main()
     std::string name;
     char b;
     int border=0;
+    char s=s;
 
 
     std::cout<<"Iveskite varda"<<std::endl;
     std::cin>>name;
 
     const int size=name.length();
+    std::string str (name);
+
 
 
     std::cout<<"Iveskite remelio dydi (1/10)"<<std::endl;
     std::cin>>border;
 
+
     int e=size+12+border*2;
+
 
 
 
@@ -43,12 +48,21 @@ std::cout<<"*";
 for(int i=0;i<border;i++){
     std::cout<<" ";
 }
+
+if(str.back()=='s'){
 std::cout<<"Sveikas, "<<name<<"!";
 for(int i=0;i<border;i++){
     std::cout<<" ";
 }
 std::cout<<"*"<<std::endl;
-
+}
+else {
+        std::cout<<"Sveika, "<<name<<"!";
+for(int i=0;i<border+1;i++){
+    std::cout<<" ";
+}
+std::cout<<"*"<<std::endl;
+}
 
 for(int j=0;j<border;j++){
         for(int i=0;i<e;i++){
