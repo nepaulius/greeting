207eb07d44f5e2fb0f750394668b89ebc5d66aac
nepaulius <pauliuscern.lt@gmail.com>
2019-02-08 14:34:48 +0200

    goal

--- main.cpp
+++ main.cpp
@@ -5,17 +5,21 @@
 
 int main()
 {
-    std::string name;
+
     char b;
     int border=0;
     char s=s;
 
 
     std::cout<<"Iveskite varda"<<std::endl;
+    std::string name;
     std::cin>>name;
 
-    const int size=name.length();
+    const int length=name.length();
     std::string str (name);
+
+    const std::string greeting = "Sveikas, " + name + "!";
+    const std::string greeting1 = "Sveika, " + name + "!";
 
 
 
@@ -23,21 +27,19 @@ int main()
     std::cin>>border;
 
 
-    int e=size+12+border*2;
 
+    int firstline=length+12+border*2;
 
-
-
-    for(int i=0;i<e;i++){
+    for(int i=0;i<firstline;i++){
         std::cout<<"*";
     }
     std::cout<<std::endl;
 
-    int g=size+11+border*2;
+    int lines=length+11+border*2;
 
      for(int j=0;j<border;j++){
-        for(int i=0;i<e;i++){
-        if(i==0||i==g)std::cout<<"*";
+        for(int i=0;i<firstline;i++){
+        if(i==0||i==lines)std::cout<<"*";
         else std::cout<<" ";
         }
         std::cout<<std::endl;
@@ -49,15 +51,20 @@ for(int i=0;i<border;i++){
     std::cout<<" ";
 }
 
+
+
+
+
+
 if(str.back()=='s'){
-std::cout<<"Sveikas, "<<name<<"!";
+std::cout<<greeting;
 for(int i=0;i<border;i++){
     std::cout<<" ";
 }
 std::cout<<"*"<<std::endl;
 }
 else {
-        std::cout<<"Sveika, "<<name<<"!";
+        std::cout<<greeting1;
 for(int i=0;i<border+1;i++){
     std::cout<<" ";
 }
@@ -65,13 +72,13 @@ std::cout<<"*"<<std::endl;
 }
 
 for(int j=0;j<border;j++){
-        for(int i=0;i<e;i++){
-        if(i==0||i==g)std::cout<<"*";
+        for(int i=0;i<firstline;i++){
+        if(i==0||i==lines)std::cout<<"*";
         else std::cout<<" ";
         }
         std::cout<<std::endl;
 }
-for(int i=0;i<e;i++){
+for(int i=0;i<firstline;i++){
         std::cout<<"*";
     }
 
